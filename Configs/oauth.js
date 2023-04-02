ae5d2ef922c14927bf21f989b5e1b0c73f7cc515
Rahul Gandhi <115460311+rahul241172@users.noreply.github.com>
2023-04-02 18:11:31 +0530

    Update oauth.js

--- Configs/oauth.js
+++ Configs/oauth.js
@@ -3,27 +3,26 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require("passport");
 const client_id = process.env.googleClientId;
 const client_key = process.env.googleClientKey;
-const { v4: uuidv4 } = require('uuid');
+
 
 
 
 passport.use(new GoogleStrategy({
     clientID: client_id,
     clientSecret: client_key,
-    callbackURL: "http://localhost:4500/auth/google/callback",
-    passReqToCallback: true
+    callbackURL: "http://localhost:4500/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
 //     console.log(profile);
     // const user_email = profile.__json.email;
     //create user by user model
     //password : uuidv4();
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-    // });
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
    
     
-    return cb(null,profile);
+//     return cb(null,profile);
   }
 ));
 
