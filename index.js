280b6821b1dfe41b094f4542777ef255335e369c
Rahul Gandhi <115460311+rahul241172@users.noreply.github.com>
2023-05-13 12:28:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,43 +33,14 @@ app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile','email'] }));
 
 app.get('/auth/google/callback', 
-  passport.authenticate('google', {failureRedirect: '/login', session:false }),
-  async function(req, res) {
-    // Successful authentication, redirect home.
-    // return value will be received here in req.user
-    // console.log(req.user);
-//      const user = {
-//       "email" : profile._json.email,
-//       "email_verified" : profile._json.email_verified,
-//       "family_name" : profile._json.family_name,
-//       "given_name" : profile._json.given_name,
-//       "name" : profile._json.name,
-//       "picture" : profile._json.picture,
-//       "sub" : profile._json.sub,
-//     }
+ passport.authenticate('google', {
+    successRedirect: '/message/frontend/messagepage.html',
+    failureRedirect: '/google/failure',
+    session: false
 
-    //check if user already exists
-    const userExist = await UserModel.find({email:req.user._json.email});
+  })
     
-    if(userExist.length > 0){
-        // console.log("User already exists");
-        
-        const token = jwt.sign({userID : userExist[0]._id}, tokenkey, { expiresIn: '1h' });
-        const reftoken = jwt.sign({userID : userExist[0]._id}, reftokenkey, { expiresIn: '7d' });
-        // res.redirect('/');
-        // console.log(token,reftoken);
-        res.redirect("/message/frontend/messagepage.html");
-        // res.send({"message":"Login Successfull","token":token,"refresh_token":reftoken});
-    }
-    else{
-        const new_user = new UserModel(req.user._json);
-        await new_user.save();
-        // console.log("User created");
-        // res.redirect('/');
-        res.send({"message":"User Registered Successfully"});
-    }
-    
-  });
+  );
 
 app.listen(4500, async () => {
     try{
